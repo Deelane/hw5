e95049a18c6d71a6ce885a859465c851e5bf8337
Deelane <Dnikoof@gmail.com>
2020-03-04 20:45:50 -0800

    first commit

---
+++ src/header.php
@@ -0,0 +1,9 @@
+<div class="navbar">
+    <ul>
+        <li><a href="index.php"></a></li>
+        <li><a href="register.php"></a></li>
+        <li><a href="login.php"></a></li>
+    </ul>
+</div>
+<?php
+
